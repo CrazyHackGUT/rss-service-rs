69d44032b01d748a456fa89eb6b790f2a285dde3
Sergey Gut <kruzefag@gmail.com>
2022-11-27 00:16:57 +0300

    More logs, fix issue with posted_at field #1

--- src/rss.rs
+++ src/rss.rs
@@ -2,7 +2,7 @@ use std::thread;
 use std::time::Duration;
 use chrono::{DateTime, Utc};
 use diesel::{ExpressionMethods, QueryDsl, RunQueryDsl};
-use log::{debug, error, info};
+use log::{debug, error, trace};
 use rss::Item;
 use crate::db::DbConnection;
 use crate::handlers::{Message, SharedVector};
@@ -13,10 +13,15 @@ use crate::models::webhook::WebHook;
 use crate::models::webhook_subscription::WebHookSubscription;
 
 pub(crate) fn start(vector: SharedVector<Message>) {
+    trace!("rss::start()");
+
     debug!("Spawning a RSS fetcher thread...");
     thread::spawn(move || {
-        info!("Spawned a RSS fetcher thread");
+        trace!("rss::start()->closure");
+
+        debug!("Spawned a RSS fetcher thread");
         loop {
+            trace!("rss::start()->closure->loop");
             handle_new_rss_posts(&vector); // vector.lock().unwrap().push(handlers::Message {body: "".to_string(), endpoint: "".to_string()});
 
             thread::sleep(Duration::from_secs(150));
@@ -26,6 +31,7 @@ pub(crate) fn start(vector: SharedVector<Message>) {
 
 fn handle_new_rss_posts(vector: &SharedVector<Message>) {
     use crate::schema::feeds::updated_at;
+    trace!("rss::handle_new_rss_posts()");
 
     let current_ts = Utc::now().naive_utc() - chrono::Duration::seconds(300);
 
@@ -43,6 +49,7 @@ fn handle_new_rss_posts(vector: &SharedVector<Message>) {
 }
 
 fn handle_feed(feed: &Feed, connection: &DbConnection, vector: &SharedVector<Message>) {
+    trace!("rss::handle_feed()");
     match feed.posts() {
         Ok(items) => {
             handle_non_existing_posts(items, &feed, connection, vector);
@@ -56,14 +63,17 @@ fn handle_feed(feed: &Feed, connection: &DbConnection, vector: &SharedVector<Mes
 fn handle_non_existing_posts(items: Vec<Item>, feed: &Feed, connection: &DbConnection, vector: &SharedVector<Message>) {
     use crate::schema::posts::dsl::posts;
     use crate::schema::posts::{feed_id, url};
+    trace!("rss::handle_non_existing_posts()");
 
     debug!("Handling non-existing posts for {}...", feed);
     for item in items {
         let item_ref = &item;
 
+        // TODO: move this shit to repository method?
         debug!("Searching post {} on forum...", item_ref.title.as_ref().unwrap_or(&"Unknown title".to_string()));
         let count = posts.filter(url.eq(item_ref.link.as_ref().unwrap().as_str())).filter(feed_id.eq(feed.id)).count()
             .first::<i64>(connection).unwrap();
+        // TODO: move this shit to repository method?
 
         match count {
             0 => handle_non_existing_post(item_ref, feed, connection, vector),
@@ -76,14 +86,24 @@ fn handle_non_existing_posts(items: Vec<Item>, feed: &Feed, connection: &DbConne
 fn handle_non_existing_post(item: &Item, feed: &Feed, connection: &DbConnection, vector: &SharedVector<Message>) {
     use crate::schema::posts::dsl::posts;
 
-    debug!("Inserting a new post (feed {}, item link {}) into database", feed.id, item.link.as_ref().unwrap());
+    trace!("rss::handle_non_existing_post()");
+    let feed_id = feed.id;
+    let title = item.title.as_ref().unwrap().to_string();
+    let url = item.link.as_ref().unwrap().to_string();
+    let posted_at = match item.pub_date.as_ref() {
+        Some(date) => DateTime::parse_from_rfc2822(date.as_str()).unwrap().naive_utc(),
+        None => Utc::now().naive_utc()
+    };
+    let received_at = Utc::now().naive_utc();
+    debug!("Inserting a new post (feed {}, item link {}) into database", feed_id, url);
+
     match diesel::insert_into(posts)
         .values(&NewPost {
-            feed_id: feed.id,
-            title: item.title.as_ref().unwrap().to_string(),
-            url: item.link.as_ref().unwrap().to_string(),
-            posted_at: DateTime::parse_from_rfc2822(item.pub_date.as_ref().unwrap().as_str()).unwrap().naive_utc(),
-            received_at: Utc::now().naive_utc()
+            feed_id,
+            title,
+            url,
+            posted_at,
+            received_at
         }).execute(connection) {
         Ok(_) => {
             debug!("Successfully inserted post {} ({}) in database", item.link.as_ref().unwrap(), feed);
@@ -97,6 +117,7 @@ fn update_feed_fetch(feed: &Feed, connection: &DbConnection) {
     use crate::schema::feeds::dsl::feeds;
     use crate::schema::feeds::{updated_at, id};
 
+    trace!("rss::update_feed_fetch({})", feed.id);
     let feeds_filtered = feeds.filter(id.eq(feed.id));
     match diesel::update(feeds_filtered)
         .set(updated_at.eq(Utc::now().naive_utc()))
@@ -110,6 +131,7 @@ fn deliver_post(item: &Item, feed: &Feed, connection: &DbConnection, vector: &Sh
     use crate::schema::webhooks_subscriptions::{feed_id, dsl::webhooks_subscriptions};
     use crate::schema::webhooks::dsl::webhooks;
 
+    trace!("rss::deliver_post()");
     let subscribers = webhooks_subscriptions.filter(feed_id.eq(feed.id))
         .inner_join(webhooks)
         .load::<(WebHookSubscription, WebHook)>(connection).unwrap();
